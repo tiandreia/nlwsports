5e51d67233a01d6626287af00167b2a6212c26f3
tiandreia <tiandreiarocha@gmail.com>
2022-09-16 13:12:06 -0300

    segunda atualização

---
+++ backend/README.md
@@ -0,0 +1,14 @@
+# Back-end
+
+## Entidades
+
+### Game
+
+#### Ad
+
+##### Casos de uso
+
+- Listagem de games com contagem de anúncios
+- Criação de novo anúncio
+- Listagem de anúncios por game
+- Buscar discord pelo ID do anúncio
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,30 @@
-import express from 'express'
+import express, { request, response } from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;
+  
   return response.json([
     {id:1, name: 'Anúncio 1'},
     {id:2, name: 'Anúncio 2'},
     {id:3, name: 'Anúncio 3'},
     {id:4, name: 'Anúncio 4'},
   ])
+})
+
+app.get('/games/:id/discord', (request, response) => {
+  // const adId = request.params.id;
+  
+  return response.json([])
 })
 
 app.listen(3333)
